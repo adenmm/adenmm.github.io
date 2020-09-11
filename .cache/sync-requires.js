877d470abe42c1626d572394c7b83cf96beb06d4
Bob <adenmm@augsburg.edu>
2020-09-10 21:46:24 -0500

    updates

--- .cache/sync-requires.js
+++ .cache/sync-requires.js
@@ -5,7 +5,6 @@ const preferDefault = m => m && m.default || m
 
 
 exports.components = {
-  "component---cache-dev-404-page-js": hot(preferDefault(require("/Users/mohamed/Documents/myWebPage/adenmm.github.io/.cache/dev-404-page.js"))),
   "component---src-pages-404-js": hot(preferDefault(require("/Users/mohamed/Documents/myWebPage/adenmm.github.io/src/pages/404.js"))),
   "component---src-pages-about-js": hot(preferDefault(require("/Users/mohamed/Documents/myWebPage/adenmm.github.io/src/pages/about.js"))),
   "component---src-pages-blog-js": hot(preferDefault(require("/Users/mohamed/Documents/myWebPage/adenmm.github.io/src/pages/blog.js"))),
